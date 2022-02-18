ba4474275febc72a5893fe60f67aac5fa6c97dc9
Hiren Kavad <hiren@chestr.io>
2022-02-18 17:20:15 +0530

    feature-doc blocks added to factory

--- contracts/RoyaltyVaultFactory.sol
+++ contracts/RoyaltyVaultFactory.sol
@@ -5,9 +5,19 @@ import {IRoyaltyVault} from "../interfaces/IRoyaltyVault.sol";
 import {RoyaltyVault} from "./RoyaltyVault.sol";
 
 contract RoyaltyVaultFactory {
+    
     address[] public vaults;
     
+    // **** Events ****
+
     event VaultCreated(address vault);
+
+    /**
+     * @dev Create RoyaltyVault
+     * @param _collectionContract address of the collection contract.
+     * @param _wethAddress address of the WETH contract.
+     * @return address of the newly created RoyaltyVault.
+     */
 
     function createVault(address _collectionContract, address _wethAddress) public returns (address) {
         RoyaltyVault newVault = new RoyaltyVault(_collectionContract,_wethAddress);
@@ -16,8 +26,13 @@ contract RoyaltyVaultFactory {
         return address(newVault);
     }
 
-    function setSpliter(address _vault, address _spliter) public {
-        IRoyaltyVault(_vault).setSplitter(_spliter);
+    /**
+    * @dev set splitter for vault.
+    * @param _vault address to vault to set splitter.
+    * @param _splitter address which needs to be set.
+    */
+    function setSpliter(address _vault, address _splitter) public {
+        IRoyaltyVault(_vault).setSplitter(_splitter);
     }
 
 }
