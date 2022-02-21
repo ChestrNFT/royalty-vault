78ae1cf29b4ecdbb4f7131f5963413672111000b
Sofiane <sofiane.ouafir@live.fr>
2022-02-21 15:12:10 -0500

    task-Add ESlint to RoyaltyVault Repo

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,16 +2,16 @@ require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-solhint');
 
-module.exports= {
+module.exports = {
   defaultNetwork: 'hardhat',
   solidity: {
     version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 2000
-      }
-    }
+        runs: 2000,
+      },
+    },
   },
   networks: {
   },
--- test/vault-tests.js
+++ test/vault-tests.js
@@ -1,67 +1,66 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const {
+  expect,
+} = require('chai');
+const { ethers } = require('hardhat');
 
 const deployRoyaltyFactory = async () => {
-    const RoyaltyVaultFactoryContract = await ethers.getContractFactory("RoyaltyVaultFactory");
-    const royaltyVaultFactory = await RoyaltyVaultFactoryContract.deploy();
-    return await royaltyVaultFactory.deployed();
+  const RoyaltyVaultFactoryContract = await ethers.getContractFactory(
+    'RoyaltyVaultFactory',
+  );
+  const royaltyVaultFactory = await RoyaltyVaultFactoryContract.deploy();
+  return royaltyVaultFactory.deployed();
 };
 
 const deployWeth = async () => {
-    const myWETHContract = await ethers.getContractFactory("WETH");
-    const myWETH = await myWETHContract.deploy();
-    return await myWETH.deployed();
+  const myWETHContract = await ethers.getContractFactory('WETH');
+  const myWETH = await myWETHContract.deploy();
+  return myWETH.deployed();
 };
 
-const createVault =async (royaltyFactory,collectionContract,wEth)=>{
-    let royaltyVault;
-    royaltyTx = await royaltyFactory.createVault(collectionContract,wEth);
-    const royaltyVaultTx = await royaltyTx.wait(1)
-    const event = royaltyVaultTx.events.find(event => event.event === 'VaultCreated');
-    [royaltyVault] = event.args;
-    return royaltyVault;
-}
-  
-describe("Creating Royalty Vault", function () {
+const createVault = async (royaltyFactory, collectionContract, wEth) => {
+  const royaltyTx = await royaltyFactory.createVault(collectionContract, wEth);
+  const royaltyVaultTx = await royaltyTx.wait(1);
+  const event = royaltyVaultTx.events.find(
+    (e) => e.event === 'VaultCreated',
+  );
+  const [royaltyVault] = event.args;
+  return royaltyVault;
+};
 
-    let royaltyFactory,wEth,royaltyVault,collectionContract,funder,account1,account2;
+describe('Creating Royalty Vault', () => {
+  let royaltyFactory;
+  let wEth;
+  let royaltyVault;
+  let collectionContract;
+  let funder;
 
-    before( async function() {
-        [
-          funder,
-          collectionContract,
-        ] = await ethers.getSigners();
-    
-        wEth = await deployWeth();
-        royaltyFactory = await deployRoyaltyFactory();
-        royaltyVault = await createVault(royaltyFactory,collectionContract.address,wEth.address);
+  before(async () => {
+    [funder, collectionContract] = await ethers.getSigners();
 
-    });
+    wEth = await deployWeth();
+    royaltyFactory = await deployRoyaltyFactory();
+    royaltyVault = await createVault(
+      royaltyFactory,
+      collectionContract.address,
+      wEth.address,
+    );
+  });
 
-    it("Should return correct RoyaltVault balance", async function () {
-        await wEth
-            .connect(funder)
-            .transfer(royaltyVault, ethers.utils.parseEther("1"));
-        const balance = await wEth.balanceOf(royaltyVault);
+  it('Should return correct RoyaltVault balance', async () => {
+    await wEth
+      .connect(funder)
+      .transfer(royaltyVault, ethers.utils.parseEther('1'));
+    const balance = await wEth.balanceOf(royaltyVault);
 
-        expect(await balance).to.eq(
-            ethers.utils.parseEther("1").toString()
-        );
+    expect(await balance).to.eq(ethers.utils.parseEther('1').toString());
+  });
 
-    });
+  it('Owner of RoyaltyVault must be RoyaltyFactory', async () => {
+    const royaltyVaultContract = await (
+      await ethers.getContractAt('RoyaltyVault', royaltyVault)
+    ).deployed();
 
-    it("Owner of RoyaltyVault must be RoyaltyFactory", async function () {
-
-        royaltyVaultContract = await (
-            await ethers.getContractAt("RoyaltyVault", royaltyVault)
-          ).deployed();
-
-        const owner = await royaltyVaultContract.owner();
-        expect(owner).to.eq(
-            royaltyFactory.address
-        );
-
-    });
-
-
+    const owner = await royaltyVaultContract.owner();
+    expect(owner).to.eq(royaltyFactory.address);
+  });
 });
